60d973638225b45a63223a6c1065fabff3d3f4d2
ldfrancis <lawrencedikeu@gmail.com>
2021-05-05 23:26:57 +0000

    Modify feature reshape for inceptionv3 extractor

--- captionwiz/model/extractor/base_extractor.py
+++ captionwiz/model/extractor/base_extractor.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 
@@ -8,6 +9,8 @@ def get_inceptionV3():
     def _extractor(img):
         img = preprocess_input(img)
         feat = base_inceptionV3(img)
+        feat = tf.reshape(feat, shape=(feat.shape[0], -1, feat.shape[3]))
+        feat = tf.squeeze(feat)
         return (feat,)
 
     return _extractor

b8e80ba712224e5e7ff1344346c4239c2d7e9e93
ldfrancis <lawrencedikeu@gmail.com>
2021-05-05 23:14:35 +0000

    fix: prevent image loader from preprocessing image

--- captionwiz/utils/img_utils.py
+++ captionwiz/utils/img_utils.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 
 import tensorflow as tf
-from tensorflow.keras.applications.inception_v3 import preprocess_input
 
 from captionwiz.utils.type import FilePath
 
@@ -10,8 +9,8 @@ def load_image(img_path: FilePath, im_size: Tuple[int, int] = None) -> tf.Tensor
     file = tf.io.read_file(img_path)
     img = tf.image.decode_jpeg(file, channels=3)
     if im_size:
-        resized_img = tf.image.resize(img)
+        resized_img = tf.image.resize(img, im_size)
     else:
         resized_img = img
-    resized_img = preprocess_input(resized_img)
-    return resized_img, img
+
+    return resized_img, img_path

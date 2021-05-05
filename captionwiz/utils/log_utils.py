59d79accc31ac7364dc2526b0e4c48e5b4a4775b
ldfrancis <lawrencedikeu@gmail.com>
2021-05-05 23:13:34 +0000

    fix: modify formatter for message logging

--- captionwiz/utils/log_utils.py
+++ captionwiz/utils/log_utils.py
@@ -1,10 +1,9 @@
 import logging
-
-import tensorflow as tf
+import sys
 
 from captionwiz.utils.config_utils import get_datetime
 
-from .constants import LOG_FILE, TENSORBOARD_LOG_DIR
+from .constants import DTIME, LOG_DIR, LOG_FILE, TENSORBOARD_LOG_DIR
 
 # logging ##
 
@@ -13,19 +12,25 @@ logger_name = "caption"
 logger = logging.getLogger(logger_name)
 
 # formatter
-formatter = logging.Formatter("%(asctime)s|%(levelname)s|%(name)s: %(message)s")
+formatter = logging.Formatter(
+    "%(module)s.py:%(lineno)d|%(levelname)s|%(name)s: %(message)s"
+)
 
 # file handler
+LOG_FILE.parent.mkdir(parents=True, exist_ok=True)
 filehandler = logging.FileHandler(LOG_FILE)
 filehandler.setFormatter(formatter)
 
 # stream handler
-streamhandler = logging.StreamHandler()
-streamhandler.setFormatter(formatter)
+streamhandlerStdOut = logging.StreamHandler(sys.stdout)
+streamhandlerStdErr = logging.StreamHandler(sys.stderr)
+streamhandlerStdOut.setFormatter(formatter)
+streamhandlerStdErr.setFormatter(formatter)
 
 # add handlers
 logger.addHandler(filehandler)
-logger.addHandler(streamhandler)
+logger.addHandler(streamhandlerStdOut)
+logger.setLevel("DEBUG")
 
 # logging levels map
 logging_levels = {
@@ -42,9 +47,19 @@ logging_levels = {
 # define log dir
 tensorboard_log_dir = TENSORBOARD_LOG_DIR
 dtime = get_datetime("%Y%m%d-%H%M%S")
-train_log_dir = tensorboard_log_dir / "{dtime}/train"
-test_log_dir = tensorboard_log_dir / "{dtime}/test"
+train_log_dir = tensorboard_log_dir / f"{dtime}/train"
+test_log_dir = tensorboard_log_dir / f"{dtime}/test"
 
 # define summary writers
-train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+train_log_dir.mkdir(parents=True, exist_ok=True)
+test_log_dir.mkdir(parents=True, exist_ok=True)
+
+
+def create_logger(name):
+    log_file = LOG_DIR / f"{DTIME}_{name}.log"
+    filehandler = logging.FileHandler(log_file)
+    filehandler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.addHandler(filehandler)
+    logger.addHandler(streamhandlerStdOut)
+    return logger
